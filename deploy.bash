4c2b40f77a5a74e30cfe8d5bc3888d9f50c67551
Daniel Schulz-Jackson <danielschulzjackson@gmail.com>
2015-07-25 18:56:26 -0500

    added ssh chmod to deploy.bash

--- deploy.bash
+++ deploy.bash
@@ -1,1 +1,21 @@
-rsync -rv --exclude '.git' . wordwide@wordwideweb.co:10x.agency/html/games/epic-title-generator
+HOST="wordwideweb.co"
+HOST_USER="wordwide"
+HOST_PATH="10x.agency/html/games/epic-title-generator"
+
+echo
+echo REMOTE SYNCHRONIZE FILES:
+rsync -rv --exclude '.git' . $HOST_USER@$HOST:$HOST_PATH
+
+echo
+echo REMOTE SET FILE PERMISSIONS:
+
+ssh $HOST_USER@$HOST bash -c "'
+
+cd $HOST_PATH
+
+find * -type d -print0 | xargs -0 chmod -v 0755 # for directories
+find . -type f -print0 | xargs -0 chmod -v 0644 # for files
+
+'"
+
+echo
